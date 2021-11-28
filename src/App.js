20c43a369c436bec61218b03e8f03025501bdff0
Rohan Vishnoi <avirohanvishnoi@gmail.com>
2021-11-29 04:44:45 +0530

    #pagination started

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import FriendList from './components/FriendList';
 function App() {
   const [inputText, setInputText] = useState("");
   const [friends, setfriends] = useState([]);
+  const [status, setStatus] = useState('all');
   return (
     <div className="App">
       <header>
      <h1>Friends List</h1>
      </header>
-     <Form friends={friends} inputText={inputText} setfriends={setfriends} setInputText={setInputText} />
+     <Form friends={friends} inputText={inputText} setfriends={setfriends} setInputText={setInputText} status={status} setStatus={setStatus} />
      <FriendList friends={friends} setfriends={setfriends} />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Form = ({setInputText, setfriends , friends , inputText}) => {
+const Form = ({setInputText, setfriends , friends , inputText ,  .}) => {
     const inputTextHandler = (e) => {
         setInputText(e.target.value);
     }
@@ -9,31 +9,17 @@ const Form = ({setInputText, setfriends , friends , inputText}) => {
         setfriends([...friends,{text : inputText , favourite: false, id: Math.random() * 1000}])
         setInputText("");
     }
-    const Sortfavstudents = (e) => {
-        let arr1 = [];
-        let arr2 = [];
-        let arr5 = [];
-       friends.forEach(element => {
-           if(element.favourite)
-           {
-               arr1.push(element);
-           } else if(!element.favourite)
-           {
-               arr2.push(element);
-           }
-       });
-
-       arr5 = arr1.concat(arr2);
-       friends =  [];
-       friends = arr5;
+    const statusHandler = (e) => {
+        console.log(e.);
     }
+   
     return(
         <form>
-            <input placeholder="Enter your freind's name" value = {inputText} onChange={inputTextHandler} type="text" className="friend-input" />
+            <input placeholder="Enter your friends name" value = {inputText} onChange={inputTextHandler} type="text" className="friend-input" />
             <button onClick={submitfriendHandler} className="friend-button" type="submit">
                 <i className ="fas fa-plus-square"></i>
             </button>
-            <button onClick={Sortfavstudents} type="submit" className="friend-button">
+            <button onClick={statusHandler} type="submit" className="friend-button">
                 <i className="fas fa-sort"></i>
             </button>
           

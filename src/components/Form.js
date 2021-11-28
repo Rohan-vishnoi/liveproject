38c7ae5f6d92cb6879bf9333acc1f268b09c0006
Rohan Vishnoi <avirohanvishnoi@gmail.com>
2021-11-29 02:10:48 +0530

    #sort

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,19 +9,34 @@ const Form = ({setInputText, setfriends , friends , inputText}) => {
         setfriends([...friends,{text : inputText , favourite: false, id: Math.random() * 1000}])
         setInputText("");
     }
+    const Sortfavstudents = (e) => {
+        let arr1 = [];
+        let arr2 = [];
+        let arr5 = [];
+       friends.forEach(element => {
+           if(element.favourite)
+           {
+               arr1.push(element);
+           } else if(!element.favourite)
+           {
+               arr2.push(element);
+           }
+       });
+
+       arr5 = arr1.concat(arr2);
+       friends =  [];
+       friends = arr5;
+    }
     return(
         <form>
-            <input value = {inputText} onChange={inputTextHandler} type="text" className="friend-input" />
+            <input placeholder="Enter your freind's name" value = {inputText} onChange={inputTextHandler} type="text" className="friend-input" />
             <button onClick={submitfriendHandler} className="friend-button" type="submit">
                 <i className ="fas fa-plus-square"></i>
             </button>
-            {/* <div className="select">
-                <select name="friends" className="filter-friend">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="uncompleted">Uncompleted</option>
-                </select>
-            </div> */}
+            <button onClick={Sortfavstudents} type="submit" className="friend-button">
+                <i className="fas fa-sort"></i>
+            </button>
+          
             </form>
     );
 
